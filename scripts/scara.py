be9ff93ada61d496513779afd6d79ec70c980897
dokyun-kim4 <donny5557@gmail.com>
2026-02-05 12:02:16 -0500

    implent DH table for SCARA

--- scripts/scara.py
+++ scripts/scara.py
@@ -1,4 +1,5 @@
 from math import *
+import pandas as pd
 import numpy as np
 import funrobo_kinematics.core.utils as ut
 from funrobo_kinematics.core import Visualizer, RobotSim
@@ -14,55 +15,25 @@ class ScaraRobot(ScaraRobotTemplate):
         th1, th2, d3 = curr_joint_values[0], curr_joint_values[1], curr_joint_values[2]
         l1, l2, l3, l4, l5 = self.l1, self.l2, self.l3, self.l4, self.l5
 
-        ## Calculate H matrix for {0} -> {1}
+        DH_TABLE = pd.DataFrame(columns=["joint", "theta", "d", "a", "alpha"])
+        DH_TABLE.joint = [i+1 for i in range(len(curr_joint_values))] # note joint indexes from 1
+        DH_TABLE.theta = [th1, th2, 0]
+        DH_TABLE.d = [l1, l3-l5, -d3]
+        DH_TABLE.a = [l2, l4, 0]
+        DH_TABLE.alpha = [0, 0, pi]
 
-        # Compute R and d separately
-        R_01 = np.array([[cos(th1), -sin(th1), 0],
-                         [sin(th1), cos(th1), 0],
-                         [0, 0, 1]
-                         ])
-        
-        d_01 = np.array([[l2*cos(th1)],
-                         [l2*sin(th1)],
-                         [l1]
-                         ])
-        
-        # Combine R and d into H
-        H_01 = np.eye(4)
-        H_01[0:3, 0:3] = R_01
-        H_01[0:3, 3:4] = d_01
-        
-        # Calculate H matrix for {1} -> {2}
-        R_12 = np.array([[cos(th2), -sin(th2), 0],
-                         [sin(th2), cos(th2), 0],
-                         [0, 0, 1]
-                         ])
-        
-        d_12 = np.array([[l4*cos(th2)],
-                         [l4*sin(th2)],
-                         [l3 - l5]
-                         ])
+        def get_H_i(i: int) -> np.ndarray:
+            curr_joint_params = DH_TABLE[DH_TABLE.joint == i].values[0][1:] # skip joint name from values
+            theta, d, a, alpha = curr_joint_params
+            return np.array([[cos(theta), -sin(theta)*cos(alpha), sin(theta)*sin(alpha), a*cos(theta)],
+                             [sin(theta), cos(theta)*cos(alpha), -cos(theta)*sin(alpha), a*sin(theta)],
+                             [0, sin(alpha), cos(alpha), d],
+                             [0, 0, 0, 1]
+                             ])
 
-        H_12 = np.eye(4)
-        H_12[0:3, 0:3] = R_12
-        H_12[0:3, 3:4] = d_12
-        
-        # Calculate H matrix for {2} -> {3}(End Effector)
-        R_23 = np.array([[1, 0, 0],
-                         [0, cos(pi), -sin(pi)],
-                         [0, sin(pi), cos(pi)]
-                         ])
-        
-        d_23 = np.array([[0],
-                         [0],
-                         [d3]
-                         ])
-        
-        H_23 = np.eye(4)
-        H_23[0:3, 0:3] = R_23
-        H_23[0:3, 3:4] = d_23
-
-        H_LIST = [H_01, H_12, H_23]
+        H_LIST = [get_H_i(i+1) for i in range(len(curr_joint_values))]
+        H_01, H_12, H_23 = H_LIST
+       
         H_EE = H_01@H_12@H_23  # Final transformation matrix for EE
 
         # Set the end effector (EE) position
