2d47084fd6ee99ff2b24275d42577115680d45cf
kenembanisi <kene.mbanisi@gmail.com>
2026-03-02 09:45:06 -0500

    add test scripts for hiwonder and kinova

--- tests/test_kinova.py
+++ tests/test_kinova.py
@@ -5,10 +5,10 @@ import yaml
 import funrobo_kinematics.core.utils as ut
 
 # Import your robot model script
-from solutions.hiwonder import FiveDOFRobot
+from solutions.kinova import KinovaRobot
 
 
-robot_model = FiveDOFRobot()
+robot_model = KinovaRobot()
 N = 100 # number of sample tries
 
 
@@ -48,29 +48,6 @@ def test_analytical_ik(joint_values):
 
     assert ut.check_valid_ik_soln(new_joint_values, ee, robot_model)
 
-
-# -----------------------------------------------------------------------------
-# Python test for numerical inverse kinematics
-# -----------------------------------------------------------------------------
-
-@pytest.mark.parametrize("joint_values", joint_values_list, ids=ids)
-def test_numerical_ik(joint_values):
-    ee, _ = robot_model.calc_forward_kinematics(joint_values, radians=True)
-
-    init_joint_values = ut.sample_valid_joints(robot_model)
-    new_joint_values = robot_model.calc_numerical_ik(ee, init_joint_values)
-
-    assert ut.check_valid_ik_soln(new_joint_values, ee, robot_model)
-
-
-# @pytest.mark.parametrize("joint_values", joint_values_list, ids=ids)
-# def test_numerical_ik_restarts(joint_values):
-#     ee, _ = robot_model.calc_forward_kinematics(joint_values, radians=True)
-
-#     init_joint_values = ut.sample_valid_joints(robot_model)
-#     new_joint_values = robot_model.calc_numerical_ik_restarts(ee, init_joint_values, 0.001)
-
-#     assert ut.check_valid_ik_soln(new_joint_values, ee, robot_model)
 
 
 # -----------------------------------------------------------------------------
