44db4a59757fe9fd4fe4cd2b89c29926e17ed9ea
dokyun-kim4 <donny5557@gmail.com>
2026-03-05 16:51:45 -0500

    refactor hardware IK script

--- scripts/hiwonder_ik.py
+++ scripts/hiwonder_ik.py
@@ -8,75 +8,12 @@ from funrobo_hiwonder.core.hiwonder import HiwonderRobot #type: ignore
 import funrobo_kinematics.core.utils as ut
 from hiwonder_model import HiWonder5DOF
 
-SQUARE_POSE = [
-                np.array([60, 230, 0]),
-                np.array([-60, 230, 0]),
-                np.array([-60, 110, 0]),
-                np.array([60, 110, 0])
-            ]
-
-SQUARE_POSE = [pose/1000 for pose in SQUARE_POSE]
-
-STAR_POSE = [
-                np.array([0, 230, 0]),
-                np.array([38, 110, 0]),
-                np.array([-60, 183, 0]),
-                np.array([60, 183, 0]),
-                np.array([-38, 110, 0])
-]
-
-STAR_POSE = [pose/1000 for pose in STAR_POSE]
-
-Ry_0T = np.array([[0, 0, 1],
-                  [0, 1, 0],
-                  [-1, 0, 0]])
-
-Rz_0T = np.array([[0, -1, 0],
-                  [1, 0, 0],
-                  [0, 0, 1]])
-
-R_0T = Ry_0T@Rz_0T
-d_0T = np.array([[-0.24], [0], [0]])
-
-SQUARE_POSE_0T = [pose @ R_0T.T + d_0T.T for pose in SQUARE_POSE]
-STAR_POSE_0T = [pose @ R_0T.T + d_0T.T for pose in STAR_POSE]
-
-print("Precomputing IK for predefined poses...")
-model = HiWonder5DOF()
-SQUARE_JOINT_ANGLES = []
-initial_guess = [0, 0, 0, 0, 0]
-p_ee = ut.EndEffector()
-pose = SQUARE_POSE_0T[0]
-p_ee.x = pose[0][0]
-p_ee.y = pose[0][1]
-p_ee.z = pose[0][2]
-SQUARE_JOINT_ANGLES.append(model.calc_numerical_ik(p_ee, initial_guess, tol=0.002, ilimit=1000))
-for pose in SQUARE_POSE_0T[1:]:
-    p_ee = ut.EndEffector()
-    p_ee.x = pose[0][0]
-    p_ee.y = pose[0][1]
-    p_ee.z = pose[0][2]
-    SQUARE_JOINT_ANGLES.append(model.calc_numerical_ik(p_ee, SQUARE_JOINT_ANGLES[-1], tol=0.002, ilimit=1000))
-
-STAR_JOINT_ANGLES = []
-for pose in STAR_POSE_0T:
-    p_ee = ut.EndEffector()
-    p_ee.x = pose[0][0]
-    p_ee.y = pose[0][1]
-    p_ee.z = pose[0][2]
-    STAR_JOINT_ANGLES.append(model.calc_numerical_ik(p_ee, [0, 0, 0, 0, 0], tol=0.002, ilimit=1000))
-
-print("Precomputing IK for predefined poses... Done.")
-
-
-
 def run_ik():
     """ Main loop that reads gamepad commands and updates the robot accordingly. """
     try:
 
         # Initialize components
         robot = HiwonderRobot()
-        model = HiWonder5DOF()
         
         control_hz = 20 
         dt = 1 / control_hz
@@ -97,15 +34,11 @@ def run_ik():
                 # Use home button to iterate through predefined poses
                 
                 if cmd.arm_home:
-                    time.sleep(0.2)
+                    time.sleep(0.2) # prevents multiple presses from registering
 
-                    # new_joint_values = model.calc_numerical_ik(p_ee, curr_joint_values, tol = 0.01, ilimit = 1000)
-                    # print("new joint values (radians):", new_joint_values)
-
-                    # new_joints_values = np.rad2deg(STAR_JOINT_ANGLES[pose_idx])
+                    # new_joints_values = np.rad2deg(STAR_JOINT_ANGLES[pose_idx % len(STAR_JOINT_ANGLES)])
                     new_joints_values = np.rad2deg(SQUARE_JOINT_ANGLES[pose_idx % len(SQUARE_JOINT_ANGLES)])
-                    # set new joint angles
-                    # Need to add 6th joint position, though not controlled via rrmc. Use same as current value
+                    # Need to add 6th joint position for gripper
                     all_joints_deg = np.append(new_joints_values, [0.0])
 
                     robot.set_joint_values(all_joints_deg, duration=dt, radians=False)
@@ -126,4 +59,60 @@ def run_ik():
         robot.shutdown_robot()
 
 if __name__ == "__main__":
+    
+    # Pre-compute IK solutions for the predefined poses to speed up execution during runtime
+    model = HiWonder5DOF()
+
+    SQUARE_POSE = [
+                    np.array([0.006, 0.230, 0]),
+                    np.array([-0.060, 0.230, 0]),
+                    np.array([-0.060, 0.110, 0]),
+                    np.array([0.060, 0.110, 0])
+                ]
+
+    STAR_POSE = [
+                    np.array([0, 0.230, 0]),
+                    np.array([0.038, 0.110, 0]),
+                    np.array([-0.060, 0.183, 0]),
+                    np.array([0.060, 0.183, 0]),
+                    np.array([-0.038, 0.110, 0])
+                ]
+
+    # Transformation matrices from robot base to target frame
+    Ry_0T = np.array([[0, 0, 1],
+                    [0, 1, 0],
+                    [-1, 0, 0]])
+
+    Rz_0T = np.array([[0, -1, 0],
+                    [1, 0, 0],
+                    [0, 0, 1]])
+
+    R_0T = Ry_0T@Rz_0T
+    d_0T = np.array([[-0.24], [0], [0]])
+
+    SQUARE_POSE_0T = [pose @ R_0T.T + d_0T.T for pose in SQUARE_POSE]
+    STAR_POSE_0T = [pose @ R_0T.T + d_0T.T for pose in STAR_POSE]
+
+    print("Precomputing IK for predefined poses...")
+    SQUARE_JOINT_ANGLES = []
+
+    initial_guess = [0.0, 0.0, pi/2, -pi/6, 0.0] * 5
+    p_ee = ut.EndEffector()
+    p_ee.x, p_ee.y, p_ee.z = SQUARE_POSE_0T[0][0]
+
+    SQUARE_JOINT_ANGLES.append(model.calc_numerical_ik(p_ee, initial_guess, tol=0.002, ilimit=1000))
+    for pose in SQUARE_POSE_0T[1:]:
+        p_ee = ut.EndEffector()
+        p_ee.x, p_ee.y, p_ee.z = pose[0]
+        SQUARE_JOINT_ANGLES.append(model.calc_numerical_ik(p_ee, SQUARE_JOINT_ANGLES[-1], tol=0.002, ilimit=1000))
+
+    p_ee = ut.EndEffector()
+    p_ee.x, p_ee.y, p_ee.z = STAR_POSE_0T[0][0]
+    STAR_JOINT_ANGLES = []
+    for pose in STAR_POSE_0T[1:]:
+        p_ee = ut.EndEffector()
+        p_ee.x, p_ee.y, p_ee.z = pose[0]
+        STAR_JOINT_ANGLES.append(model.calc_numerical_ik(p_ee, [0.0] * 6, tol=0.002, ilimit=1000))
+
+    print("Precomputing IK for predefined poses... Done.")
     run_ik()
